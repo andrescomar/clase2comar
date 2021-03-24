f6d7d384f261b3dc0b5d85cc10dc3b8d07b8ee9f
andrescomar <andrescomar@hotmail.com>
2021-03-24 16:56:35 -0300

    Primer archivo clase 2

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let unNumero = 5;
+
+if ( unNumero > 10) {
+    alert("Numero alto")
+} else {
+    alert("Numero menor a 10");
+}
+
+let nombre = prompt("Ingrese su nombre");
+
+if (nombre == "") {
+    alert("Ingrese su nombre por favor")
+} else {
+    alert("Hola señor " + nombre)
+}
+
+let myAge = prompt("Ingrese su edad")
+
